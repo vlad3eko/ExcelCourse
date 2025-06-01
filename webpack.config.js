601cf0d1c1e700f4f41e8fb165490cf9d47dee17
unknown <vlad3eko@gmail.com>
2025-06-01 11:57:48 +0300

    study 01.06.05

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const {CopyWebpackPlugin} = require("copy-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
@@ -11,6 +11,15 @@ module.exports = {
 		// hash = autoload new version
 		filename: 'bundle.[hash].js',
 		path: path.resolve(__dirname, 'dist')
+	},
+	// AutoLoad .js
+	resolve: {
+		extensions: ['.js'],
+		// reduction path for file\folder
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'core': path.resolve(__dirname, 'src/core')
+		}
 	},
 	// Add plug for HTML
 	plugins: [
@@ -24,5 +33,8 @@ module.exports = {
 				to: path.resolve(__dirname, 'dist')
 			}
 		]),
+		new MiniCssExtractPlugin({
+			filename: 'bundle.[hash].css'
+		})
 	],
 }
