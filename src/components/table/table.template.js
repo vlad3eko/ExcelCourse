5e8bffd8adcd0d803d43884d439d2766992ecdb3
vlad <vlad3eko@gmail.com>
2025-06-15 13:52:16 +0300

    Finish Framework Edit class 'Table'.

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,23 +2,21 @@ const CODES = {
   A: 65,
   Z: 90,
 }
-
-function createCell(content) {
+function toCell(content) {
   return `
-  <div class="cell" contenteditable>A1</div>
+  <div class="cell" contenteditable>${content}</div>
   `
 }
-
 function toColumn(col) {
   return `
     <div class="column">${col}</div>
   `
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
     <div class="row">
-      <div class="row-info"></div>
+      <div class="row-info">${index ? index : ''}</div>
       <div class="row-data">${content}</div>
     </div>
   `
@@ -32,13 +30,13 @@ export function createTable(rowsCount = 20) {
   const colsCount = CODES.Z - CODES.A + 1
 
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('')
-
   const rows = []
 
-  rows.push(createRow(cols))
+  rows.push(createRow(null, cols))
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow())
+    const cell = new Array(colsCount).fill('').map(toCell).join('')
+    rows.push(createRow(i + 1, cell))
   }
   return rows.join('')
 }
