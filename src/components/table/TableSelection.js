efa024619f1b69cfd2b989bf98cc30c65f02d914
vlad <vlad3eko@gmail.com>
2025-06-24 23:18:14 +0300

    05.08 table-selected group

--- src/components/table/TableSelection.js
+++ src/components/table/TableSelection.js
@@ -2,12 +2,14 @@ export class TableSelection {
   static className = 'selected'
   constructor() {
     this.group = []
+    this.current = null
   }
 
   select($el) {
     this.clear()
     this.group.push($el)
     $el.addClass(TableSelection.className)
+    this.current = $el
   }
 
   clear() {
@@ -15,5 +17,10 @@ export class TableSelection {
     this.group = []
   }
 
-  selectGroup() {}
+  selectGroup($group = []) {
+    this.clear()
+
+    this.group = $group
+    this.group.forEach(($el) => $el.addClass(TableSelection.className))
+  }
 }
