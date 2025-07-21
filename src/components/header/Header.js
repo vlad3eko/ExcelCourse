91c2a602ad4b108c3d89ec762eebcc3db87e877c
vlad <vlad3eko@gmail.com>
2025-07-22 00:35:09 +0300

    22.07 Finish Excel

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,6 +3,7 @@ import { changeTitle } from '@/redux/actions'
 import { $ } from '@core/dom'
 import { defaultTitle } from '@/constants'
 import { debounce } from '@core/utils'
+import { ActiveRouter } from '@core/routes/ActiveRouter'
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -10,7 +11,7 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
-      listeners: ['input'],
+      listeners: ['input', 'click'],
       ...options,
     })
   }
@@ -26,17 +27,32 @@ export class Header extends ExcelComponent {
 
       <div>
 
-        <div class="button">
-          <i class="material-symbols-outlined">delete</i>
+        <div class="button" data-button="remove">
+          <i class="material-symbols-outlined" data-button="remove">delete</i>
         </div>
 
-        <div class="button">
-          <i class="material-symbols-outlined">exit_to_app</i>
+        <div class="button" data-button="exit">
+          <i class="material-symbols-outlined" data-button="exit">exit_to_app</i>
         </div>
 
       </div>
     `
   }
+
+  onClick(event) {
+    const $target = $(event.target)
+    if ($target.data.button === 'remove') {
+      const decision = confirm(`Вы действительно хотите удалить таблицу?`)
+
+      if (decision) {
+        localStorage.removeItem('excel: ' + ActiveRouter.param)
+        ActiveRouter.navigate('')
+      }
+    } else if ($target.data.button === 'exit') {
+      ActiveRouter.navigate('')
+    }
+  }
+
   onInput(event) {
     const $target = $(event.target)
     this.$dispatch(changeTitle($target.text()))
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { APPLY_STYLE, CHANGE_STYLES, CHANGE_TEXT, CHANGE_TITLE, CURRENT_STYLE, TABLE_RESIZE } from './types'
+import { APPLY_STYLE, CHANGE_STYLES, CHANGE_TEXT, CHANGE_TITLE, TABLE_RESIZE, UPDATE_DATE } from './types'
 
 // Action Creator
 export function tableResize(data) {
@@ -12,6 +12,12 @@ export function changeText(data) {
   return {
     type: CHANGE_TEXT,
     data,
+  }
+}
+
+export function updateDate() {
+  return {
+    type: UPDATE_DATE,
   }
 }
 
